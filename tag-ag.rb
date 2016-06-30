4d63139e1fafd24b4c11d4a20be81ed03d32f16f
Aleks Kamko <aykamko@gmail.com>
2016-06-29 19:45:47 -0700

    auditing for official homebrew support

--- tag-ag.rb
+++ tag-ag.rb
@@ -1,31 +1,32 @@
-require 'formula'
-
-HOMEBREW_TAG_VERSION='1.2.0'
+HOMEBREW_TAG_VERSION="1.2.0".freeze
 class TagAg < Formula
-  desc 'Instantly jump to your ag matches.'
-  homepage 'https://github.com/aykamko/tag'
+  desc "Instantly jump to your ag matches."
+  homepage "https://github.com/aykamko/tag"
   url "https://github.com/aykamko/tag/releases/download/#{HOMEBREW_TAG_VERSION}/tag_darwin_amd64.zip"
-  sha256 'b696243ac49053e60b1747b26c9d9731dad2534be660bf229fa959e5a4c4bc91'
+  sha256 "b696243ac49053e60b1747b26c9d9731dad2534be660bf229fa959e5a4c4bc91"
 
   version HOMEBREW_TAG_VERSION
-  head 'https://github.com/aykamko/tag.git', :branch => 'master'
+  head "https://github.com/aykamko/tag.git", :branch => "master"
 
-  depends_on 'ag' => :build
+  depends_on "the_silver_searcher" => :build
   if build.head?
-    depends_on 'hg' => :build
-    depends_on 'go' => :build
+    depends_on "hg" => :build
+    depends_on "go" => :build
   end
 
   def install
     go_build if build.head?
-    bin.install 'tag'
+    bin.install "tag"
   end
 
   def go_build
-    ENV['GOPATH'] = buildpath
-    system 'go', 'get', 'github.com/fatih/color'
-    mkdir_p buildpath/'src/github.com/aykamko'
-    ln_s buildpath, buildpath/'src/github.com/aykamko/tag'
-    system 'go', 'build', '-o', 'tag'
+    ENV["GOPATH"] = buildpath
+    system "go", "get", "github.com/fatih/color"
+    mkdir_p buildpath/"src/github.com/aykamko"
+    ln_s buildpath, buildpath/"src/github.com/aykamko/tag"
+    system "go", "build", "-o", "tag"
+  end
+
+  test do
   end
 end
