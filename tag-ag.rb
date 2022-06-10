edbdf3c072972da71a12b2706d44a00c063b801d
Chris Thielen <github@sandgnat.com>
2022-06-09 15:39:13 -1000

    Always build tag-ag from source

--- tag-ag.rb
+++ tag-ag.rb
@@ -2,26 +2,24 @@ HOMEBREW_TAG_VERSION="v1.4.0".freeze
 class TagAg < Formula
   desc "Instantly jump to your ag or ripgrep matches."
   homepage "https://github.com/aykamko/tag"
-  url "https://github.com/aykamko/tag/releases/download/#{HOMEBREW_TAG_VERSION}/tag_darwin_amd64.zip"
-  sha256 "d5cfe843e0953e1f440a82e488d54b0a9f51671a70bba74f7417107c788f1796"
+  url "https://github.com/aykamko/tag/archive/refs/tags/#{HOMEBREW_TAG_VERSION}.tar.gz"
+  sha256 "d3a02466e600634cf0ffff9ad8c5f70eba97e44758edf35cc4efbda9cbfdff9a"
 
   version HOMEBREW_TAG_VERSION
   head "https://github.com/aykamko/tag.git", :branch => "master"
 
   depends_on "the_silver_searcher" => :build
-  if build.head?
-    depends_on "hg" => :build
-    depends_on "go" => :build
-  end
+  depends_on "hg" => :build
+  depends_on "go" => :build
 
   def install
-    go_build if build.head?
+    go_build
     bin.install "tag"
   end
 
   def go_build
-    ENV["GOPATH"] = buildpath
-    system "go", "get", "github.com/fatih/color"
+    system "go", "mod", "init", "main"
+    system "go", "mod", "tidy"
     mkdir_p buildpath/"src/github.com/aykamko"
     ln_s buildpath, buildpath/"src/github.com/aykamko/tag"
     system "go", "build", "-o", "tag"
